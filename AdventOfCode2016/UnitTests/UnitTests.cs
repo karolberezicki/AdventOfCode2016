f96cc65b4b147a1a4a48ba017686fc0f9075f15c
Karol Berezicki <Senoy05@hotmail.com>
2016-11-30 22:20:57 +0100

    Init

---
+++ AdventOfCode2016/UnitTests/UnitTests.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace UnitTests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void AreUnitTestsWorking()
+        {
+            bool theTruth = true;
+            Assert.True(theTruth);
+        }
+    }
+}
