22631a10c54296339eaa1cca835e735249098f32
Karol Berezicki <Senoy05@hotmail.com>
2016-12-01 06:39:26 +0100

    Day01, Part one

--- AdventOfCode2016/Day01/Program_01.cs
+++ AdventOfCode2016/Day01/Program_01.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,79 @@ namespace Day01
     {
         public static void Main(string[] args)
         {
+            string source = File.ReadAllText(@"..\..\input.txt");
+            source = source.Remove(source.Length - 1);
+
+            source = source.Replace(" ", "");
+
+            List<string> list = source.Split(',').ToList();
+
+            var currentOrientation = Orientation.North;
+            int X = 0;
+            int Y = 0;
+
+            int[] adfdsfds = new int[1000];
+
+
+            foreach (var item in list)
+            {
+
+                var moveLength = int.Parse(new string(item.Where(c => char.IsDigit(c)).ToArray()));
+
+                var changeOrientation = currentOrientation.ToString() + item[0];
+
+                switch (changeOrientation)
+                {
+                    case "NorthR":
+                        currentOrientation = Orientation.East;
+                        Y += moveLength;
+                        break;
+                    case "NorthL":
+                        currentOrientation = Orientation.West;
+                        Y -= moveLength;
+                        break;
+                    case "EastR":
+                        currentOrientation = Orientation.South;
+                        X -= moveLength;
+                        break;
+                    case "EastL":
+                        currentOrientation = Orientation.North;
+                        X += moveLength;
+                        break;
+                    case "SouthR":
+                        currentOrientation = Orientation.West;
+                        Y -= moveLength;
+                        break;
+                    case "SouthL":
+                        currentOrientation = Orientation.East;
+                        Y += moveLength;
+                        break;
+                    case "WestR":
+                        currentOrientation = Orientation.North;
+                        X += moveLength;
+                        break;
+                    case "WestL":
+                        currentOrientation = Orientation.South;
+                        X -= moveLength;
+                        break;
+                    default:
+                        break;
+                }
+
+            }
+
+            
+
+
         }
+    }
+
+
+    public enum Orientation
+    {
+        North,
+        East,
+        South,
+        West
     }
 }
