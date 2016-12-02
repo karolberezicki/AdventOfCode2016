5c3a9a4066023393b24e1497db961de47e2d2a53
Karol Berezicki <karolberezicki@gmail.com>
2016-12-02 17:16:41 +0100

    Refactor Day01, Added Day02

--- AdventOfCode2016/Day01/Program_01.cs
+++ AdventOfCode2016/Day01/Program_01.cs
@@ -13,70 +13,108 @@ namespace Day01
         {
             string source = File.ReadAllText(@"..\..\input.txt");
             source = source.Remove(source.Length - 1);
-
             source = source.Replace(" ", "");
-
-            List<string> list = source.Split(',').ToList();
-
-            var currentOrientation = Orientation.North;
-            int X = 0;
-            int Y = 0;
-
-            int[] adfdsfds = new int[1000];
+            List<string> intructions = source.Split(',').ToList();
+            
+            Elves elves = new Elves();
 
 
-            foreach (var item in list)
+            foreach (string instruction in intructions)
             {
 
-                var moveLength = int.Parse(new string(item.Where(c => char.IsDigit(c)).ToArray()));
+                int moveLength = int.Parse(new string(instruction.Where(c => char.IsDigit(c)).ToArray()));
 
-                var changeOrientation = currentOrientation.ToString() + item[0];
+                string changeOrientation = elves.Orientation.ToString() + instruction[0];                
 
-                switch (changeOrientation)
+                if (changeOrientation == "NorthR" || changeOrientation == "SouthL")
                 {
-                    case "NorthR":
-                        currentOrientation = Orientation.East;
-                        Y += moveLength;
-                        break;
-                    case "NorthL":
-                        currentOrientation = Orientation.West;
-                        Y -= moveLength;
-                        break;
-                    case "EastR":
-                        currentOrientation = Orientation.South;
-                        X -= moveLength;
-                        break;
-                    case "EastL":
-                        currentOrientation = Orientation.North;
-                        X += moveLength;
-                        break;
-                    case "SouthR":
-                        currentOrientation = Orientation.West;
-                        Y -= moveLength;
-                        break;
-                    case "SouthL":
-                        currentOrientation = Orientation.East;
-                        Y += moveLength;
-                        break;
-                    case "WestR":
-                        currentOrientation = Orientation.North;
-                        X += moveLength;
-                        break;
-                    case "WestL":
-                        currentOrientation = Orientation.South;
-                        X -= moveLength;
-                        break;
-                    default:
-                        break;
+                    elves.Move(Orientation.East, moveLength);
                 }
+                else if (changeOrientation == "NorthL" || changeOrientation == "SouthR")
+                {
+                    elves.Move(Orientation.West, moveLength);
+                }
+                else if (changeOrientation == "EastR" || changeOrientation == "WestL")
+                {
+                    elves.Move(Orientation.South, moveLength);
+                }
+                else if (changeOrientation == "EastL" || changeOrientation == "WestR")
+                {
+                    elves.Move(Orientation.North, moveLength);
+                }                
 
             }
 
-            
+            Console.WriteLine("Part one = {0}", (Math.Abs(elves.X) + Math.Abs(elves.Y)));
+            KeyValuePair<string, int> firstPointVisitedTwice = elves.VisitedPoints.First(a => a.Value >= 2);
+            string[] firstPointVisitedTwiceValues = firstPointVisitedTwice.Key.Split(',');
+            Console.WriteLine("Part two = {0}", (Math.Abs(int.Parse(firstPointVisitedTwiceValues[0])) + Math.Abs(int.Parse(firstPointVisitedTwiceValues[1]))));
 
+            Console.WriteLine(firstPointVisitedTwice.Key);
+            Console.ReadLine();
 
         }
     }
+
+    public class Elves
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Orientation Orientation { get; set; }
+
+        public Dictionary<string,int> VisitedPoints { get; set; }
+
+        public Elves()
+        {
+            X = 0;
+            Y = 0;
+            Orientation = Orientation.North;
+            VisitedPoints = new Dictionary<string, int>();
+                
+        }
+
+        public void Move(Orientation newOrientation, int moveLength)
+        {
+            Orientation = newOrientation;
+
+            if (Orientation == Orientation.North || Orientation == Orientation.South)
+            {
+                int changeXSign = newOrientation == Orientation.North ? 1 : -1;
+
+                for (int i = 0; i < moveLength; i++)
+                {
+                    X += changeXSign;
+                    AddVisitedPoint(X, Y);
+                }
+            }
+            else
+            {
+                int changeYSign = newOrientation == Orientation.East ? 1 : -1;
+
+                for (int i = 0; i < moveLength; i++)
+                {
+                    Y += changeYSign;
+                    AddVisitedPoint(X, Y);
+                }
+            }
+            
+        }
+
+        public void AddVisitedPoint(int visitedX, int visitedY)
+        {
+            string key = string.Format("{0},{1}", visitedX, visitedY);
+            if (VisitedPoints.ContainsKey(key))
+            {
+                VisitedPoints[key] = VisitedPoints[key] + 1;
+            }
+            else
+            {
+                VisitedPoints[key] = 1;
+            }            
+        }
+    }
+
 
 
     public enum Orientation
---
+++ AdventOfCode2016/Day02/Program_02.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day02
+{
+    public class Program_02
+    {
+        public static void Main(string[] args)
+        {
+            string source = File.ReadAllText(@"..\..\input.txt");
+            source = source.Remove(source.Length - 1);
+            //source = source.Replace(" ", "");
+            List<string> intructions = source.Split('\n').ToList();
+
+
+            string passcode = "";
+
+            foreach (string intruction in intructions)
+            {
+
+                int number = 5;
+
+                for (int i = 0; i < intruction.Length; i++)
+                {
+                    int change = 0;
+
+                    switch (intruction[i])
+                    {
+                        case 'U':
+                            change = -3;
+                            break;
+                        case 'D':
+                            change = 3;
+                            break;
+                        case 'L':
+                            change = -1;
+                            break;
+                        case 'R':
+                            change = 1;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (number < 4 && change == -3)
+                    {
+                        continue;
+                    }
+
+                    if (number > 6 && change == 3)
+                    {
+                        continue;
+                    }
+
+                    if (number % 3 == 0 && change == 1)
+                    {
+                        continue;
+                    }
+
+                    if ((number +2) % 3 == 0 && change == -1)
+                    {
+                        continue;
+                    }
+
+                    number = number + change;
+                }
+
+                passcode = passcode + number;                
+
+
+            }
+
+
+
+            string passcode2 = "";
+
+
+            char[] keypad = {
+
+                '0', '0', '1', '0', '0',
+                '0', '2', '3', '4', '0',
+                '5', '6', '7', '8', '9',
+                '0', 'A', 'B', 'C', '0',
+                '0', '0', 'D', '0', '0'
+            };
+
+            int number2 = 10;
+            foreach (string intruction in intructions)
+            {
+                
+                for (int i = 0; i < intruction.Length; i++)
+                {
+                    int change = 0;
+
+                    switch (intruction[i])
+                    {
+                        case 'U':
+                            change = -5;
+                            break;
+                        case 'D':
+                            change = 5;
+                            break;
+                        case 'L':
+                            change = -1;
+                            break;
+                        case 'R':
+                            change = 1;
+                            break;
+                        default:
+                            break;
+                    }
+
+                    if (number2 < 4 && change == -5)
+                    {
+                        continue;
+                    }
+
+                    if (number2 > 19 && change == 5)
+                    {
+                        continue;
+                    }
+
+                    if (keypad[number2 + change] == '0')
+                    {
+                        continue;
+                    }
+
+                    number2 = number2 + change;
+
+
+                }
+                passcode2 = passcode2 + keypad[number2];
+
+            }
+
+
+        }
+    }
+}
