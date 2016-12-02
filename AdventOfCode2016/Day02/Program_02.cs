4e2f17f5c03c8ec0f9fe328b8f0d843c404a3b54
Karol Berezicki <karolberezicki@gmail.com>
2016-12-02 18:44:22 +0100

    Day02 Refactor

--- AdventOfCode2016/Day02/Program_02.cs
+++ AdventOfCode2016/Day02/Program_02.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day02
 {
@@ -13,96 +11,60 @@ namespace Day02
         {
             string source = File.ReadAllText(@"..\..\input.txt");
             source = source.Remove(source.Length - 1);
-            //source = source.Replace(" ", "");
-            List<string> intructions = source.Split('\n').ToList();
+            List<string> instructions = source.Split('\n').ToList();
 
+            // Keypads are surrounded with zeros to skip handling out of bounds exception
 
-            string passcode = "";
-
-            foreach (string intruction in intructions)
-            {
-
-                int number = 5;
-
-                for (int i = 0; i < intruction.Length; i++)
-                {
-                    int change = 0;
-
-                    switch (intruction[i])
-                    {
-                        case 'U':
-                            change = -3;
-                            break;
-                        case 'D':
-                            change = 3;
-                            break;
-                        case 'L':
-                            change = -1;
-                            break;
-                        case 'R':
-                            change = 1;
-                            break;
-                        default:
-                            break;
-                    }
-
-                    if (number < 4 && change == -3)
-                    {
-                        continue;
-                    }
-
-                    if (number > 6 && change == 3)
-                    {
-                        continue;
-                    }
-
-                    if (number % 3 == 0 && change == 1)
-                    {
-                        continue;
-                    }
-
-                    if ((number +2) % 3 == 0 && change == -1)
-                    {
-                        continue;
-                    }
-
-                    number = number + change;
-                }
-
-                passcode = passcode + number;                
-
-
-            }
-
-
-
-            string passcode2 = "";
-
-
-            char[] keypad = {
-
-                '0', '0', '1', '0', '0',
-                '0', '2', '3', '4', '0',
-                '5', '6', '7', '8', '9',
-                '0', 'A', 'B', 'C', '0',
-                '0', '0', 'D', '0', '0'
+            char[] keypad1 = {
+                 '0', '0', '0', '0', '0',
+                 '0', '1', '2', '3', '0',
+                 '0', '4', '5', '6', '0',
+                 '0', '7', '8', '9', '0',
+                 '0', '0', '0', '0', '0',
+                 '0', '0', '0', '0', '0',
             };
 
-            int number2 = 10;
-            foreach (string intruction in intructions)
+            string passcode1 = GetPasscode(keypad1, 12, instructions);
+
+            char[] keypad2 = {
+               '0', '0', '0', '0', '0', '0', '0',
+               '0', '0', '0', '1', '0', '0', '0',
+               '0', '0', '2', '3', '4', '0', '0',
+               '0', '5', '6', '7', '8', '9', '0',
+               '0', '0', 'A', 'B', 'C', '0', '0',
+               '0', '0', '0', 'D', '0', '0', '0',
+               '0', '0', '0', '0', '0', '0', '0'
+            };
+
+            string passcode2 = GetPasscode(keypad2, 22, instructions);
+
+            Console.WriteLine("Part one = {0}", passcode1);
+            Console.WriteLine("Part two = {0}", passcode2);
+            Console.ReadLine();
+        }
+
+
+        public static string GetPasscode(char[] keypad, int startLocation, List<string> instructions)
+        {
+            string passcode = "";
+
+            int verticalChange = (int)Math.Sqrt(keypad.Length);
+
+            int currentLocation = startLocation;
+            foreach (string instruction in instructions)
             {
-                
-                for (int i = 0; i < intruction.Length; i++)
+
+                for (int i = 0; i < instruction.Length; i++)
                 {
                     int change = 0;
 
-                    switch (intruction[i])
+                    switch (instruction[i])
                     {
                         case 'U':
-                            change = -5;
+                            change = -verticalChange;
                             break;
                         case 'D':
-                            change = 5;
+                            change = verticalChange;
                             break;
                         case 'L':
                             change = -1;
@@ -113,31 +75,18 @@ namespace Day02
                         default:
                             break;
                     }
-
-                    if (number2 < 4 && change == -5)
+                    // Skip change, if it's outside of actual keypad
+                    if (keypad[currentLocation + change] == '0')
                     {
                         continue;
                     }
-
-                    if (number2 > 19 && change == 5)
-                    {
-                        continue;
-                    }
-
-                    if (keypad[number2 + change] == '0')
-                    {
-                        continue;
-                    }
-
-                    number2 = number2 + change;
-
+                    currentLocation = currentLocation + change;
 
                 }
-                passcode2 = passcode2 + keypad[number2];
-
+                passcode = passcode + keypad[currentLocation];
             }
 
-
+            return passcode;
         }
     }
 }
