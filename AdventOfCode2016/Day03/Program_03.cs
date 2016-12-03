43ef0ef51be0112a51498fbc9f63e57d26a2e4ae
Karol Berezicki <karolberezicki@gmail.com>
2016-12-03 08:08:11 +0100

    Day03

---
+++ AdventOfCode2016/Day03/Program_03.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day03
+{
+    public class Program_03
+    {
+        public static void Main(string[] args)
+        {
+            string source = File.ReadAllText(@"..\..\input.txt");
+            source = source.Remove(source.Length - 1);
+            List<string> instructions = source.Split('\n').ToList();
+
+            int countTrianglesPartOne = instructions
+                .Select(i => SplitPoints(i))
+                .Select(t => new Triangle(t))
+                .Count(t => t.IsValidTriangle);
+
+            int countTrianglesPartTwo = 0;
+            for (int i = 0; i < instructions.Count; i = i + 3)
+            {
+                List<string> lineOne = SplitPoints(instructions[i]);
+                List<string> lineTwo = SplitPoints(instructions[i + 1]);
+                List<string> lineThree = SplitPoints(instructions[i + 2]);
+
+                List<Triangle> triangles = new List<Triangle>
+                {
+                     new Triangle(lineOne[0], lineTwo[0], lineThree[0]),
+                     new Triangle(lineOne[1], lineTwo[1], lineThree[1]),
+                     new Triangle(lineOne[2], lineTwo[2], lineThree[2]),
+                };
+
+                countTrianglesPartTwo += triangles.Count(t => t.IsValidTriangle);
+            }
+
+            Console.WriteLine("Part one = {0}", countTrianglesPartOne);
+            Console.WriteLine("Part two = {0}", countTrianglesPartTwo);
+            Console.ReadLine();
+
+        }
+
+        public static List<string> SplitPoints(string instruction)
+        {
+            return instruction.Split(' ').Where(dd => !string.IsNullOrWhiteSpace(dd)).ToList();
+        }
+    }
+}
---
+++ AdventOfCode2016/Day03/Triangle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Day03
+{
+    public class Triangle
+    {
+        public Triangle(List<string> points) : this(points[0], points[1], points[2])
+        {
+                        
+        }
+
+        public Triangle(string a, string b, string c)
+        {
+            A = int.Parse(a);
+            B = int.Parse(b);
+            C = int.Parse(c);
+        }
+
+        public int A { get; set; }
+        public int B { get; set; }
+        public int C { get; set; }
+
+        public bool IsValidTriangle
+        {
+            get { return (A + B > C && A + C > B && B + C > A); }
+        }
+    }
+}
