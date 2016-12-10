72dcc5662d11e2faef2d8524357bf461089478e0
Karol Berezicki <karolberezicki@gmail.com>
2016-12-10 08:22:07 +0100

    Day09, Part two

--- AdventOfCode2016/Day09/Program_09.cs
+++ AdventOfCode2016/Day09/Program_09.cs
@@ -16,6 +16,11 @@ namespace Day09
             StringBuilder sb = Decompress(source);
 
             int partOneDecompressedLength = sb.Length;
+            long partTwoDecompressedLength = DecompressLengthV2(source);
+
+            Console.WriteLine("Part one = {0}", partOneDecompressedLength);
+            Console.WriteLine("Part two = {0}", partTwoDecompressedLength);
+            Console.ReadLine();
 
         }
 
@@ -66,6 +71,48 @@ namespace Day09
             }
 
             return sb;
+        }
+
+        public static long DecompressLengthV2(string source)
+        {
+            if (!source.Contains('('))
+            {
+                return source.Length;
+            }
+                
+            long decompressedLength = 0;
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                if (source[i] != '(')
+                {
+                    decompressedLength++;
+                    continue;
+                }
+
+                StringBuilder marker = new StringBuilder();
+                marker.Append(source[i]);
+                for (int j = i + 1; j <= source.Length; j++)
+                {
+                    marker.Append(source[j]);
+                    if (source[j] == ')')
+                    {
+                        break;
+                    }
+                }
+                string[] markerValues = marker.ToString().Split('(', ')')[1].Split('x');
+
+                int subsequentCharacters = int.Parse(markerValues[0]);
+                int repeatDecompressed = int.Parse(markerValues[1]);
+
+                int repeat = repeatDecompressed.ToString().Length + subsequentCharacters.ToString().Length + 3;
+                string partToDecompress = source.Substring(i + repeat, subsequentCharacters);
+
+                decompressedLength += DecompressLengthV2(partToDecompress) * repeatDecompressed;
+                i += repeat + subsequentCharacters - 1;
+            }
+            
+            return decompressedLength;
         }
     }
 }
