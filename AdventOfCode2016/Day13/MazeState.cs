643d3eb00402dd3c5267aabfadc88227d6416b01
Karol Berezicki <karolberezicki@gmail.com>
2016-12-18 21:38:38 +0100

    Better solution for Day 13

--- AdventOfCode2016/Day13/MazeState.cs
+++ AdventOfCode2016/Day13/MazeState.cs
@@ -1,20 +1,19 @@
 using System.Diagnostics;
+using System.Drawing;
 
 namespace Day13
 {
-    [DebuggerDisplay("X = {X}, Y = {Y},  Distance = {Distance}")]
+    [DebuggerDisplay("X = {Position.X}, Y = {Position.Y},  Distance = {Distance}")]
 
     public class MazeState
     {
         public MazeState(int x, int y, int distance)
         {
-            X = x;
-            Y = y;
+            Position = new Point(x, y); 
             Distance = distance;
         }
 
-        public int X { get; }
-        public int Y { get; }
+        public Point Position { get; }
         public int Distance { get; }
 
         public override bool Equals(object obj)
@@ -23,11 +22,11 @@ namespace Day13
                 return false;
 
             MazeState mazeState = (MazeState)obj;
-            return (X == mazeState.X) && (Y == mazeState.Y);
+            return (Position == mazeState.Position) && (Distance == mazeState.Distance);
         }
         public override int GetHashCode()
         {
-            return X ^ Y;
+            return Position.GetHashCode() ^ Distance.GetHashCode();
         }
     }
 }
--- AdventOfCode2016/Day13/Program13.cs
+++ AdventOfCode2016/Day13/Program13.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 
 namespace Day13
@@ -14,20 +15,59 @@ namespace Day13
             //CreateMaze(input, maze);
             //DisplayMaze(maze);
 
-            int goalX = 31;
-            int goalY = 39;
-            
-            // Add start position
-            HashSet<MazeState> mazeStates = new HashSet<MazeState>{ new MazeState(1, 1, 0) };
+            Queue<MazeState> statesQueue = new Queue<MazeState>(new[] { new MazeState(1, 1, 0) });
+            MazeState finishState = null;
 
-            FindPossibleStates(mazeStates, input, goalX, goalY);
+            HashSet<Point> placesInRangeOf50 = new HashSet<Point>();
 
-            int minimalDistance = mazeStates.First(s => s.X == goalX && s.Y == goalY).Distance;
+            Point target = new Point(31, 39);
 
-            int placesInRangeOf50 = mazeStates.Count(s => s.Distance <= 50);
+            while (statesQueue.Count > 0)
+            {
+                MazeState currentState = statesQueue.Dequeue();
 
-            Console.WriteLine("Part one = {0}", minimalDistance);
-            Console.WriteLine("Part two = {0}", placesInRangeOf50);
+                if (currentState.Distance <= 50 && !placesInRangeOf50.Contains(currentState.Position))
+                {
+                    placesInRangeOf50.Add(currentState.Position);
+                }
+
+                if (currentState.Position == target)
+                {
+                    finishState = finishState ?? currentState;
+                }
+                else
+                {
+                    HashSet<MazeState> possibleMoves = new HashSet<MazeState>
+                    {
+                        new MazeState(currentState.Position.X + 1, currentState.Position.Y, currentState.Distance + 1),
+                        new MazeState(currentState.Position.X - 1, currentState.Position.Y, currentState.Distance + 1),
+                        new MazeState(currentState.Position.X, currentState.Position.Y + 1, currentState.Distance + 1),
+                        new MazeState(currentState.Position.X, currentState.Position.Y - 1, currentState.Distance + 1)
+                    };
+
+                    if (finishState != null)
+                    {
+                        possibleMoves =
+                            new HashSet<MazeState>(possibleMoves.Where(move => move.Distance < finishState.Distance));
+                    }
+
+                    foreach (MazeState newState in possibleMoves)
+                    {
+                        if (IsOpenSpace(input, newState.Position.Y, newState.Position.X) && !statesQueue.Contains(newState))
+                        {
+                            statesQueue.Enqueue(newState);
+                        }
+                    }
+
+                }
+            }
+
+
+            int partOne = finishState?.Distance ?? 0;
+            int partTwo = placesInRangeOf50.Count;
+
+            Console.WriteLine("Part one = {0}", partOne);
+            Console.WriteLine("Part two = {0}", partTwo);
             Console.ReadLine();
 
 
@@ -65,50 +105,15 @@ namespace Day13
 
         private static bool IsOpenSpace(int input, int y, int x)
         {
+            if (x < 0 || y < 0)
+            {
+                return false;
+            }
+
             int equation = x * x + 3 * x + 2 * x * y + y + y * y;
             string res = Convert.ToString(equation + input, 2);
             int count = res.Length - res.Replace("1", "").Length;
             return count % 2 == 0;
         }
-
-        private static void FindPossibleStates(ISet<MazeState> mazeStates, int input, int goalX, int goalY)
-        {
-            if (mazeStates.FirstOrDefault(s => s.X == goalX && s.Y == goalY) != null)
-            {
-                return;
-            }
-
-            HashSet<MazeState> statesToAdd = new HashSet<MazeState>();
-
-            foreach (MazeState mazeState in mazeStates)
-            {
-                HashSet<MazeState> possibleMoves = new HashSet<MazeState>
-                {
-                    new MazeState(mazeState.X + 1, mazeState.Y, mazeState.Distance + 1),
-                    new MazeState(mazeState.X - 1, mazeState.Y, mazeState.Distance + 1),
-                    new MazeState(mazeState.X, mazeState.Y + 1, mazeState.Distance + 1),
-                    new MazeState(mazeState.X, mazeState.Y - 1, mazeState.Distance + 1)
-                };
-
-                foreach (MazeState newState in possibleMoves)
-                {
-                    if (IsOpenSpace(input, newState.Y, newState.X) 
-                        && newState.X >= 0 
-                        && newState.Y >= 0 
-                        && !mazeStates.Contains(newState))
-                    {
-                        statesToAdd.Add(newState);
-                    }
-                }
-            }
-
-            mazeStates.UnionWith(statesToAdd);
-
-            if (statesToAdd.Count > 0)
-            {
-                FindPossibleStates(mazeStates, input, goalX, goalY);
-            }
-        }
-
     }
 }
