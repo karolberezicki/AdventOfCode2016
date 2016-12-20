40ed91c09781e584f135e5bec70cb91149fe6f3b
Karol Berezicki <karolberezicki@gmail.com>
2016-12-20 18:21:44 +0100

    Day19 Part2, Part1 simpler solution

--- AdventOfCode2016/Day19/Program19.cs
+++ AdventOfCode2016/Day19/Program19.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Day19
 {
@@ -14,58 +9,22 @@ namespace Day19
             const int input = 3001330;
 
             int partOne = Part1(input);
+            int partTwo = Part2(input);
 
             Console.WriteLine("Part one = {0}", partOne);
-            //Console.WriteLine("Part two = {0}", partTwo);
+            Console.WriteLine("Part two = {0}", partTwo);
             Console.ReadLine();
         }
 
         private static int Part1(int input)
         {
-            List<Elf> elves = new List<Elf>();
-            for (int i = 0; i < input; i++)
-            {
-
-                elves.Add(new Elf { Number = i + 1, Presents = 1 });
-
-            }
-
-
-            while (elves.Count > 1)
-            {
-                for (int i = 0; i < elves.Count; i++)
-                {
-
-                    if (elves[i].Presents == 0)
-                    {
-                        continue;
-                    }
-
-                    if (i == elves.Count - 1)
-                    {
-                        elves[i].Presents += elves[0].Presents;
-                        elves[0].Presents = 0;
-                    }
-                    else
-                    {
-                        elves[i].Presents += elves[i + 1].Presents;
-                        elves[i + 1].Presents = 0;
-                    }
-
-                }
-
-                elves = elves.Where(e => e.Presents > 0).ToList();
-
-            }
-
-            return elves.First().Number;
+            string binary = Convert.ToString(input, 2);
+            return Convert.ToInt32(binary.Substring(1, binary.Length - 1) + binary[0], 2);
         }
-    }
 
-
-    public class Elf
-    {
-        public int Number { get; set; }
-        public int Presents { get; set; }
+        private static int Part2(int input)
+        {
+            return input - (int)Math.Pow(3, (int)Math.Log(input, 3));
+        }
     }
 }
